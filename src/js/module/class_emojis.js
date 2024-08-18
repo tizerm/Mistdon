806fd75765305f13de15f5fd13bacbc9c72fedc2
tizerm <t.max.jp@gmail.com>
2024-08-19 01:46:35 +0900

    Patch-1.4.1: Misskeyのカスタム絵文字を非同期でバインドする箇所で同じ絵文字が複数回使用されているときにマークアップがバグる不具合を修正.

--- src/js/module/class_emojis.js
+++ src/js/module/class_emojis.js
@@ -159,7 +159,7 @@ class Emojis {
             const emoji_map = auth.emojis.emoji_map
             return shortcodes.reduce((str, code) => {
                 const emoji = emoji_map.get(code)
-                return str.replace(new RegExp(emoji.shortcode, 'g'),
+                return str.replace(new RegExp(`(?<!alt=")${emoji.shortcode}`, 'g'),
                     `<img src="${emoji.url}" class="inline_emoji" alt="${emoji.shortcode}"/>`)
             }, text)
         } else { // 認証アカウント外のインスタンスの場合は現地のAPIから絵文字を取得
@@ -181,8 +181,8 @@ class Emojis {
             // 取得に成功したショートコードを抜き出して置換処理を実行
             return await Promise.allSettled(emoji_promises).then(results => {
                 return results.filter(res => res.status == 'fulfilled').map(res => res.value)
-                    .reduce((str, emoji) => str.replace(new RegExp(emoji.shortcode, 'g'),
-                        `<img src="${emoji.url}" class="inline_emoji" alt=":${emoji.shortcode}:"/>`), text)
+                    .reduce((str, emoji) => str.replace(new RegExp(`(?<!alt=")${emoji.shortcode}`, 'g'),
+                        `<img src="${emoji.url}" class="inline_emoji" alt="${emoji.shortcode}"/>`), text)
             })
         }
     }
